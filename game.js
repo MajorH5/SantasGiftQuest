ce6e74e6216db9e02438baba59248e3776ffdf60
Habib <73318032+MajorH5@users.noreply.github.com>
2023-10-26 22:54:49 -0400

    content preloading

--- game.js
+++ game.js
@@ -1,10 +1,42 @@
 import { SantasGiftQuest } from "./lib/santasGiftQuest.js";
 
-const canvas = document.getElementById("canvas");
+const isDev = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 const game = new SantasGiftQuest(canvas);
 
+function waitUserInteraction () {
+    return new Promise((resolve) => {
+        document.addEventListener('mousedown', resolve, { once: true });
+        document.addEventListener('touchstart', resolve, { once: true});
+        document.addEventListener('keydown', resolve, { once: true});
+    });
+}
+
+function drawText (text, x = canvas.width / 2, y = canvas.height / 2) {
+    context.font = '20px sans-serif';
+    context.fillStyle = 'white';
+    context.textAlign = 'center';
+
+    context.fillText(text, x, y);
+}
+
 (async function () {
+    if (!isDev) {
+        canvas.width = SantasGiftQuest.DEFAULT_CANVAS_SIZE.x;
+        canvas.height = SantasGiftQuest.DEFAULT_CANVAS_SIZE.y;
+
+        drawText('Preloading content, please wait. This wont take long.');
+    }
+    
     await game.init();
+    
+    if (!isDev) {
+        drawText('Click to start the game.');
+        await waitUserInteraction();
+    }
+
     game.globalStart();
     globalThis.game = game;
 })();
